2477c3b947bd9a2ea300b0d73820908f2de73d82
mkpappu <mkpappu1@gmail.com>
2024-01-16 16:00:26 -0500

    updated conf.php and works with PHP v7.4.30

--- exp/conf.php
+++ exp/conf.php
@@ -12,39 +12,62 @@ $experimentAlias = 'stroop';
 $language = 'english';
 // $language = 'french';
 // $language = 'german';
+?>
+<script type="text/javascript">
+    const language = "<?php echo $language; ?>";
+</script>
 
+<?php
 // SET SUBJECT IDENTIFICATION
-if (isset($_GET["workerId"])) {
-  $workerId = $_GET["workerId"];
-  $subjectId = $_GET["workerId"];
-}
+  if (isset($_GET["workerId"])) {
+  $workerId = isset($_GET["workerId"]) ? $_GET["workerId"] : null;
+  $subjectId = isset($_GET["workerId"]) ? $_GET["workerId"] : null;
+  }
 
-if (isset($_GET["PROLIFIC_PID"])) {
-  $PROLIFIC_PID = $_GET["PROLIFIC_PID"];
-  $subjectId = $_GET["PROLIFIC_PID"];
-}
-
-if (isset($_GET["participantId"])) {
-  $participantId = $_GET["participantId"];
-  $subjectId = $_GET["participantId"];
-}
-
-if (isset($_GET["src_subject_id"])) {
-  $src_subject_id = $_GET["src_subject_id"];
-  $subjectId = $_GET["src_subject_id"];
-  
+  if (isset($_GET["PROLIFIC_PID"])) {
+  $PROLIFIC_PID = isset($_GET["PROLIFIC_PID"]) ? $_GET["PROLIFIC_PID"] : null;
+  $subjectId = isset($_GET["PROLIFIC_PID"]) ? $_GET["PROLIFIC_PID"] : null;
+  $workerId = null;
+  } else {
+    $PROLIFIC_PID = null;
+  }
+  if (isset($_GET["participantId"])) {
+  $participantId = isset($_GET["participantId"]) ? $_GET["participantId"] : null;
+  $subjectId = isset($_GET["participantId"]) ? $_GET["participantId"] : null;
+  $workerId = null;
+  } else {
+    $participantId = null;
+  }
+  if (isset($_GET["src_subject_id"])) {
+  $src_subject_id = isset($_GET["src_subject_id"]) ? $_GET["src_subject_id"] : null;
+  $subjectId = isset($_GET["src_subject_id"]) ? $_GET["src_subject_id"] : null;
   // these are omnibus data base variables which will get passed from participant portal
-  $studyId = $_GET["studyId"];
-  $candidateId = $_GET["candidateId"];
+  $studyId = isset($_GET["studyId"]) ? $_GET["studyId"] : null;
+  $candidateId = isset($_GET["candidateId"]) ? $_GET["candidateId"] : null;
   // these are NDA required variables which will get passed from participant portal
-  $subjectKey = $_GET["subjectkey"];
-  $consortId = $_GET["src_subject_id"];
-  $sexAtBirth = $_GET["sex"];
-  $institutionAlias = $_GET["site"];
-  $ageInMonths = $_GET["interview_age"];
-  $groupStatus = $_GET["phenotype"];
-  $visit = $_GET["visit"];
-}
+  $subjectKey = isset($_GET["subjectkey"]) ? $_GET["subjectkey"] : null;
+  $consortId = isset($_GET["src_subject_id"]) ? $_GET["src_subject_id"] : null;
+  $sexAtBirth = isset($_GET["sex"]) ? $_GET["sex"] : null;
+  $institutionAlias = isset($_GET["site"]) ? $_GET["site"] : null;
+  $ageInMonths = isset($_GET["interview_age"]) ? $_GET["interview_age"] : null;
+  $groupStatus = isset($_GET["phenotype"]) ? $_GET["phenotype"] : null;
+  $visit = isset($_GET["visit"]) ? $_GET["visit"] : null;
+  $workerId = null;
+  } else {
+    $src_subject_id = null;
+    $candidateId = null;
+    $studyId = null;
+    $subjectKey = null;
+    $consortId = null;
+    $sexAtBirth = null;
+    $institutionAlias = null;
+    $ageInMonths = null;
+    $groupStatus = null;
+    $visit = null;
+  }
+
+  
+
 
 /**
  * Get the hash of the current git HEAD
@@ -69,7 +92,7 @@ if (isset($_GET["src_subject_id"])) {
     const participantId = "<?php echo $participantId; ?>";
     let src_subject_id = "<?php echo $src_subject_id; ?>";
     let subjectId = "<?php echo $subjectId; ?>";
-    const language = "<?php echo $language; ?>";
+
     const adminEmail = "joshua.kenney@yale.edu";
     const feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/kamin.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
     // these are NDA required variables which will get passed from participant portal 
@@ -80,6 +103,7 @@ if (isset($_GET["src_subject_id"])) {
     const ageAtAssessment = "<?php echo $ageInMonths?>";
     const groupStatus = "<?php echo $groupStatus?>";
     const visit = "<?php echo $visit?>";
+
 </script>
 
 
