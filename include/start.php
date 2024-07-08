9f04b1e71c6c28f1615df0b07a837b1d9c9563ca
joshkenney <joshuagkenney@gmail.com>
2024-07-08 18:37:22 -0400

    added start.php for ESB

---
+++ include/start.php
@@ -0,0 +1,11 @@
+<!-- consent.php -->
+<div id="consentContent">
+  <!-- Your consent form content here -->
+</div>
+
+<script>
+  // Call your JavaScript function here
+  $.getScript('exp/timeline.js', function() {
+    validateStart();
+  });
+</script>
