6f37805489fe9c55665ef4de6bba8ec2d737dd63
joshkenney <joshuagkenney@gmail.com>
2026-01-23 14:31:35 -0500

    using var.js instead of var.php

--- tmp/var.js
+++ tmp/var.js
@@ -1,14 +1,6 @@
-<!-- Purpose of var.js: To include all global variables (e.g., trialIterator) -->
-<!-- And any php server side logic -->
+// Purpose of var.js: To include all global variables (e.g., trialIterator)
+// And any php server side logic
 
-<?php
-// include any php code here
-?>
-
-
-<!-- define all javascript globals here -->
-
-<script>
 "use strict";
 
 let trialIterator = 0;
@@ -25,6 +17,3 @@ const trials = stroopVariables.length;
 const blocks = 1;
 
 const totalTrials = trials * blocks;
-
-</script>
-
